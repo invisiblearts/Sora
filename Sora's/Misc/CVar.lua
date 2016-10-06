e01a103c6d09ed5c99e49dd6f9c8ca707709afb1
neavo <neavo@neavo.me>
2016-10-06 13:35:11 +0800

    CVar : More CVar Setting

--- Sora's/Misc/CVar.lua
+++ Sora's/Misc/CVar.lua
@@ -1,9 +1,18 @@
 ﻿-- Engine
 -- local S, C, L, DB = unpack(select(2, ...))
-
 -- Begin
 local function OnPlayerLogin(self, event, ...)
+    -- 反和谐
     SetCVar("overrideArchive", 0)
+    
+    -- 姓名板最远显示距离
+    SetCVar("nameplateMaxDistance", 40)
+    
+    -- 还原经典战斗文字效果
+    SetCVar("floatingCombatTextCombatDamageDirectionalScale", 0)
+    
+    -- 还原经典切换目标（Tab）范围
+    SetCVar("TargetPriorityAllowAnyOnScreen", 0)
 end
 
 -- Event
